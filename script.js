34146ab9106566e6188e7425cfd0eff7bb0ef8f3
monu <sawlanimohnish@gmail.com>
2023-08-05 01:48:12 +0530

    CSS modified and animation improved

--- script.js
+++ script.js
@@ -7,20 +7,6 @@ document.addEventListener("mousemove", function (dets) {
   blur.style.top = dets.y - 100 + "px";
 });
 
-var h4all = document.querySelectorAll(".nav h4");
-h4all.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
-    crsr.style.scale = 3;
-    crsr.style.border = "1px solid #631cde";
-    crsr.style.backgroundColor = "transparent";
-  });
-  elem.addEventListener("mouseleave", function () {
-    crsr.style.scale = 1;
-    crsr.style.border = "0px solid #631cde";
-    crsr.style.backgroundColor = "#631cde";
-  });
-});
-
 gsap.to(".nav", {
   backgroundColor: "black",
   height: "3.5rem",
@@ -28,7 +14,7 @@ gsap.to(".nav", {
   scrollTrigger: {
     trigger: ".nav",
     scroller: "body",
-    // markers: true,
+    markers: true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1,
@@ -63,11 +49,11 @@ gsap.from(".card", {
   scale: 0.8,
   // opacity: 0,
   duration: 0.6,
-  stagger: 0.1,
+  // stagger: 0.1,
   scrollTrigger: {
     trigger: ".card",
     scroller: "body",
-    // markers: true,
+    markers: true,
     start: "top 70%",
     end: "top 65%",
     scrub: 1,
